f299292691f3d182833aeb1e4992a3c19cb48943
Joe Linogao <linogaoj@tcd.ie>
2024-07-08 21:40:19 +0100

    Created basic menu with switch case

--- README.md
+++ README.md
@@ -1,8 +1,20 @@
-# Word Similarity Search (Java 17) 
-Word similarity search created with Java 17 to allow users to rank words or phrases based on a 50d vector dataset. 
-This project allows users to provide a 50d vector dataset of word embeddings and a word/phrase they would like to parse.
-From there, the program provides the highest ranked words based on similarity using an approach of the user's choosing. 
+# Word Similarity Search (Java 17)
+Word similarity search created with **Java 17** to allow users to rank words or phrases based on a 50d vector dataset. 
+This project allows users to provide a [**50d vector dataset of word embeddings**](https://nlp.stanford.edu/projects/glove/) 
+and a word/phrase they would like to parse. From there, the program provides the highest ranked words based on similarity 
+using an approach of the user's choosing.
+
+## State-of-the-Art Features
+1. **Bring your own weights:** Along with the default 50d dataset we provide, you are able to specify your own 50d
+dataset for parsing and ranking
+2. **Parse single words or sentences:** Parse a single word or an entire sentence, and the system will try to provide
+the best result for you
+3. 
+
 The following similarity algorithms include:
 - Dot Product
 - Euclidean Distance
 - Cosine Distance
+
+## How To Run 
+
--- src/ie/atu/sw/CLIMenu.java
+++ src/ie/atu/sw/CLIMenu.java
@@ -1,4 +1,51 @@
 package ie.atu.sw;
 
+import java.util.Scanner;
+
+import static java.lang.System.in;
+import static java.lang.System.out;
+
 public class CLIMenu {
+    private String configLocation;
+    private final Scanner s;
+    private boolean running = true;
+    private int topWords = 10;
+    public CLIMenu (){
+        s = new Scanner(in);
+    }
+
+    public void start(){
+        do {
+            showOptions();
+            int choice = s.nextInt();
+            switch (choice) {
+                case 1 -> out.println("option 1");
+                case 2 -> out.println("option 2");
+                case 3 -> out.println("option 3");
+                case 4 -> out.println("option 4. Top words is: " + topWords);
+                case 5 -> out.println("option 5");
+                case 6 -> out.println("option 6");
+                case 7 -> {
+                    running = false;
+                    out.println("Existing program...");
+                }
+                default -> out.println("Not an option. Please try again");
+
+            }
+        }while(running);
+    }
+
+    private void showOptions(){
+        out.println("^^^^ ^^^^ ^^^^ ^^^^ ^^^^ ^^^^");
+        out.println("^^^^ Student Manager Menu ^^^^");
+        out.println("^^^^ ^^^^ ^^^^ ^^^^ ^^^^ ^^^^");
+        out.println("1) Provide file path for 50d word embeddings dataset");
+        out.println("2) Provide file path for output (default /out.txt)");
+        out.println("3) Select similarity search algorithm");
+        out.println("4) Change number of words to show in similarity ranking");
+        out.println("5) Enable/Disable weight details");
+        out.println("6) Begin word similarity search");
+        out.println("7) Quit");
+        out.println("Select an option [1-7]");
+    }
 }
