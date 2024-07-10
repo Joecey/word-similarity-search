7e8c0e48c83962311b0e790f2264a4c4bb92d3c8
Joe Linogao <linogaoj@tcd.ie>
2024-07-10 21:48:10 +0100

    Added new variables into CLIMenu object

--- src/ie/atu/sw/CLIMenu.java
+++ src/ie/atu/sw/CLIMenu.java
@@ -5,8 +5,20 @@ import java.util.Scanner;
 import static java.lang.System.in;
 import static java.lang.System.out;
 
+
 public class CLIMenu {
-    private String configLocation;
+    private enum AlgorithmOptions{
+        // apply strategy pattern
+        COSINE,
+        DOT,
+        EUCLIDEAN
+
+
+    }
+    private String wordWeightsLocation;
+    private String outputLocation = "./out.txt";
+    private int numberOfResults = 10;
+    private boolean showWeights = false;
     private final Scanner s;
     private boolean running = true;
     private int topWords = 10;
@@ -23,7 +35,7 @@ public class CLIMenu {
                 case 2 -> out.println("option 2");
                 case 3 -> out.println("option 3");
                 case 4 -> out.println("option 4. Top words is: " + topWords);
-                case 5 -> out.println("option 5");
+                case 5 -> setShowWeights(!showWeights);
                 case 6 -> out.println("option 6");
                 case 7 -> {
                     running = false;
@@ -35,15 +47,20 @@ public class CLIMenu {
         }while(running);
     }
 
+    private void setShowWeights(boolean showWeights) {
+        this.showWeights = showWeights;
+        out.println("Show weight details: " + showWeights + "\n");
+    }
+
     private void showOptions(){
         out.println(Colours.ANSI_PURPLE + "^^^^\t^^^^\t^^^^\t^^^^\t^^^^\t^^^^");
         out.println("\t\t\tWord Similarity Search\t\t");
         out.println("^^^^\t^^^^\t^^^^\t^^^^\t^^^^\t^^^^");
         out.println(Colours.ANSI_BLUE + "1) Provide file path for 50d word embeddings dataset");
-        out.println("2) Provide file path for output (default /out.txt)");
-        out.println("3) Select similarity search algorithm");
-        out.println("4) Change number of words to show in similarity ranking");
-        out.println(Colours.ANSI_YELLOW +"5) Enable/Disable weight details");
+        out.println("2) Provide file path for output (current location: " +outputLocation+ ")");
+        out.println("3) Select similarity search algorithm (COSINE, DOT, EUCLIDEAN)");
+        out.println("4) Change number of words to show in similarity ranking (current amount: " + numberOfResults + ")");
+        out.println(Colours.ANSI_YELLOW +"5) Enable/Disable weight details ("+showWeights+")");
         out.println(Colours.ANSI_GREEN + "6) Begin word similarity search");
         out.println(Colours.ANSI_RED +  "7) Quit" + Colours.ANSI_RESET);
         out.println("Select an option [1-7]");
