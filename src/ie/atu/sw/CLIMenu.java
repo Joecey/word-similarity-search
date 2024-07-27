13fd0d7c5d2d2ef1ff407c9abde31e64068b79e8
Joe Linogao <linogaoj@tcd.ie>
2024-07-27 19:13:46 +0100

    Added ability to change the amount of top words you can see

--- src/ie/atu/sw/CLIMenu.java
+++ src/ie/atu/sw/CLIMenu.java
@@ -11,16 +11,12 @@ public class CLIMenu {
     private enum AlgorithmOptions {
         /* apply strategy pattern - call on specific functions when needed.
         Call the required algorithm from different class */
-        COSINE,
-        DOT,
-        EUCLIDEAN
+        COSINE, DOT, EUCLIDEAN
     }
 
     public enum LogLevel {
         // specify your fields here
-        INFO("[INFO]: "),
-        WARN("[WARN]: "),
-        ERROR("[ERROR]: ");
+        INFO("[INFO]: "), WARN("[WARN]: "), ERROR("[ERROR]: ");
 
         // specify field values
         private final String message;
@@ -37,7 +33,6 @@ public class CLIMenu {
     }
 
     private String outputLocation = "./out.txt";
-    private int numberOfResults = 10;
     private boolean showWeights = false;
     private boolean running = true;
     private int topWords = 10;
@@ -48,22 +43,30 @@ public class CLIMenu {
     public CLIMenu() {
     }
 
-    public void setCurrentModel(ModelWeights currentModel) {
+    private void setCurrentModel(ModelWeights currentModel) {
         this.currentModel = currentModel;
+    }
+
+    private void setTopWords(int topWords) {
+        this.topWords = topWords;
+    }
+
+    private void setOutputLocation(String outputLocation) {
+        this.outputLocation = outputLocation;
     }
 
     public void start() throws InterruptedException {
         do {
             TimeUnit.MILLISECONDS.sleep(500);
             showOptions();
-            try{
+            try {
                 Scanner choiceScanner = new Scanner(in);
                 int choice = choiceScanner.nextInt();
                 switch (choice) {
                     case 1 -> loadWeightDataset();
                     case 2 -> setNewOutputFile();
                     case 3 -> out.println("option 3");
-                    case 4 -> out.println("option 4. Top words is: " + topWords);
+                    case 4 -> updateTopWords();
                     case 5 -> setShowWeights(!showWeights);
                     case 6 -> beginWordSimilaritySearch();
                     case 7 -> {
@@ -72,7 +75,7 @@ public class CLIMenu {
                     }
                     default -> out.println("Not an option. Please try again");
                 }
-            } catch (InputMismatchException err){
+            } catch (InputMismatchException err) {
                 out.println(LogLevel.WARN.getMessage() + "Input provided is not a valid integer. Please try again");
             }
 
@@ -87,12 +90,13 @@ public class CLIMenu {
         out.println(Colours.ANSI_BLUE + "1) Provide file path for 50d word embeddings dataset");
         out.println("2) Provide file path for output (current location: " + outputLocation + ")");
         out.println("3) Cycle similarity search algorithm (COSINE, DOT, EUCLIDEAN)");
-        out.println("4) Change number of words to show in similarity ranking (current amount: " + numberOfResults + ")");
+        out.println("4) Change number of words to show in similarity ranking (current amount: " + topWords + ")");
         out.println(Colours.ANSI_YELLOW + "5) Enable/Disable weight details (" + showWeights + ")");
         out.println(Colours.ANSI_GREEN + "6) Begin word similarity search");
         out.println(Colours.ANSI_RED + "7) Quit" + Colours.ANSI_RESET);
         out.print("Select an option [1-7]: ");
     }
+
 
     private void loadWeightDataset() {
         // TODO: Create exception handling here and os file check
@@ -113,26 +117,6 @@ public class CLIMenu {
         }
     }
 
-    private void setShowWeights(boolean showWeights) {
-        this.showWeights = showWeights;
-        out.println("Show weight details: " + showWeights + "\n");
-    }
-
-    private void beginWordSimilaritySearch() {
-        // TODO: Make this it's own class or new static class instead
-        if (currentModel == null) {
-            out.println(LogLevel.WARN.getMessage()+ "There is no dataset loaded. Use option 1) to load an appropriate file");
-        } else {
-
-            out.println(LogLevel.INFO.getMessage() + "wordList and weightList loaded correctly \n");
-
-        }
-    }
-
-    private void setOutputLocation(String outputLocation) {
-        this.outputLocation = outputLocation;
-    }
-
     private void setNewOutputFile() {
         try {
             Scanner outputFileScanner = new Scanner(in);
@@ -148,6 +132,35 @@ public class CLIMenu {
             out.println(LogLevel.ERROR.getMessage() + "An issue has occurred while setting output file: " + err);
         }
 
+    }
+
+    private void updateTopWords() {
+        try {
+            Scanner topWordsScanner = new Scanner(in);
+            out.print("Provide the amount of words you would like displayed in ranking: ");
+            int newTopWords = topWordsScanner.nextInt();
+            setTopWords(newTopWords);
+        } catch (InputMismatchException inputErr) {
+            out.println(LogLevel.WARN.getMessage() + "That is not a valid integer. Please provide an integer value for top words");
+        } catch (Exception err) {
+            out.println(LogLevel.ERROR.getMessage() + "An error has occurred while updating top words. Please try again");
+        }
+    }
+
+    private void setShowWeights(boolean showWeights) {
+        this.showWeights = showWeights;
+        out.println("Show weight details: " + showWeights + "\n");
+    }
+
+    private void beginWordSimilaritySearch() {
+        // TODO: Make this it's own class or new static class instead
+        if (currentModel == null) {
+            out.println(LogLevel.WARN.getMessage() + "There is no dataset loaded. Use option 1) to load an appropriate file");
+        } else {
+
+            out.println(LogLevel.INFO.getMessage() + "wordList and weightList loaded correctly \n");
+
+        }
     }
 
 }
--- src/ie/atu/sw/ModelWeights.java
+++ src/ie/atu/sw/ModelWeights.java
@@ -30,17 +30,17 @@ public class ModelWeights {
 
     public ModelWeights(String filePath) throws Exception {
         File modelFile = new File(filePath);
-        if (!modelFile.exists()){
+        if (!modelFile.exists()) {
             throw new Exception("This file does not exist!");
-        } else{
-            try{
-                
+        } else {
+            try {
+
                 // once we are all good, introduce buffer reader to read modelFile
                 BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(modelFile)));
                 String currentLine = null;
                 int count = 0;
-                while((currentLine= br.readLine()) != null){
-                    if(count ==0){
+                while ((currentLine = br.readLine()) != null) {
+                    if (count == 0) {
                         processLine(currentLine);
 
                     }
@@ -49,29 +49,30 @@ public class ModelWeights {
                 br.close();
                 System.out.println(count + " words have been successfully loaded");
 
-            } catch (Exception err){
+            } catch (Exception err) {
                 throw new Exception("[WARN]: There was an issue processing the file" + err);
             }
         }
     }
-    private void processLine(String currentLineBuffer) throws Exception{
-        try{
-            String trimmed = currentLineBuffer.replaceAll("\\s+","");
+
+    private void processLine(String currentLineBuffer) throws Exception {
+        try {
+            String trimmed = currentLineBuffer.replaceAll("\\s+", "");
             String[] trimmedSplit = trimmed.split(",");
             System.out.println(trimmedSplit.length);
             appendWordArray(trimmedSplit);
             appendWeightMatrix(trimmedSplit);
-        } catch (Exception err){
+        } catch (Exception err) {
             throw new Exception(err);
         }
 
     }
-    
-    private void appendWordArray(String[] currentLineArray) throws Exception{
+
+    private void appendWordArray(String[] currentLineArray) throws Exception {
         System.out.println("Word array: " + currentLineArray[0]);
     }
 
-    private void appendWeightMatrix(String[] currentLineArray) throws Exception{
+    private void appendWeightMatrix(String[] currentLineArray) throws Exception {
         System.out.println("Weight matrix: " + currentLineArray[1]);
     }
 
